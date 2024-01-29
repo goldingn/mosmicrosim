c5e55f474b89e9cb81ccc9e1e93e1c90bd431669
Nick Golding <goldingn35@gmail.com>
2024-01-29 19:34:59 +0800

    flesh out spline interpolation of daily climate data from terraclimate

--- R/query_terraclimate.R
+++ R/query_terraclimate.R
@@ -2,15 +2,15 @@
 library(ncdf4)
 library(lubridate)
 
-# get the URL to the terraclimate data
-terraclimate_url <- function(variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad", "soil")) {
+# get the URL to the terraclimate monthly summary data
+terraclimate_url <- function(variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad")) {
   variable <- match.arg(variable)
   base_url <- "http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_terraclimate_%s_1958_CurrentYear_GLOBE.nc#fillmismatch"
   sprintf(base_url, variable)
 }
 
-# open the connection to terraclimate
-terraclimate_open <- function(variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad", "soil")) {
+# open the connection to terraclimate monthly data
+terraclimate_open <- function(variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad")) {
   url <- terraclimate_url(variable = variable)
   ncdf4::nc_open(url)
 }
@@ -34,7 +34,7 @@ terraclimate_available_indices <- function() {
 # terraclimate
 terraclimate_fetch <- function(
     slice,
-    variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad", "soil")) {
+    variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad")) {
   # open a connection with the required cube, and close it again on exiting
   # this function
   variable <- match.arg(variable)
@@ -48,9 +48,9 @@ terraclimate_fetch <- function(
                    count = slice$count)
 }
 
-# find the nearest lat and long in terra climate the user's. Check the requested
-# coordinate agains the available set. If there isn't one within 1/48 of a
-# degree, get one slightly further (basing this on NicheMapR's logic)
+# find the nearest lat and long in terra climate to the user's. Check the
+# requested coordinate against the available set. If there isn't one within 1/48
+# of a degree, get one slightly further (basing this on NicheMapR's logic)
 nearest_coord <- function(value, available) {
   error <- abs(available - value)
   acceptable <- error < 1/48
@@ -76,8 +76,8 @@ monthly_summary_days <- function(dates) {
   min_date <- lubridate::floor_date(min(dates), unit = "month")
   # find the start of month immediately after the last date
   max_date <- lubridate::ceiling_date(max(dates), unit = "month")
-  # roll back to the ends of the previous months (how terraclimate summarises)
-  seq(min_date, max_date, by = "1 month") - 1
+  # sequence of all the first dates of the months in between
+  seq(min_date, max_date, by = "1 month")
 }
 
 # error nicely if the user provides inaccessible dates
@@ -87,10 +87,10 @@ check_dates <- function(dates, terraclimate_available) {
 
   # get the summary dates available and the time period they cover (the start of
   # the first date summarised to the end of the last)
-  available_dates <- days_since_1900()[terraclimate_available$times]
-  coverage_min <- lubridate::floor_date(min(available_dates),
-                                        unit = "month")
-  coverage_max <- max(available_dates)
+  available_dates <- days_since_1900()[terraclimate_available$times+1]
+  coverage_min <- min(available_dates)
+  coverage_max <- lubridate::ceiling_date(max(available_dates),
+                                        unit = "month") - 1
 
   # error if the request cannot be fulfilled
   if (min(dates) < coverage_min | max(dates) > coverage_max) {
@@ -119,20 +119,21 @@ terraclimate_build_slice <- function(longitude,
   # them from terraclimate
   check_dates(dates, available)
 
-  # the timeseries is summarised by month and indexed by the last day of the
+  # the timeseries is summarised by month and indexed by the first day of the
   # month, so find all summary dates relevant to our target dates
   required_summary_dates <- monthly_summary_days(dates)
 
-  # the terraclimate temporal index is integer days since the start of 1900 so
-  # find our month-end dates as indices
-  time_index_1900 <- match(required_summary_dates, days_since_1900())
+  # the terraclimate temporal index is integer days since the first day of 1900
+  # (ie. indexed from 0, unlike R's indexing from 1, so subtract 1) so find our
+  # month-start dates as indices
+  time_index_1900 <- match(required_summary_dates, days_since_1900()) - 1
 
   # I'm insecure
   if (!all(time_index_1900 %in% available$times)) {
     stop("Some date error. It's Nick's fault, please tell him.")
   }
 
-  # now index the elements of the NCDF array
+  # now index the elements of the NCDF array - accounting for indexing from 0 in NCDF
   time_index <- match(time_index_1900, available$times)
 
   # Find the nearest terraclimate lat longs to the ones we want
@@ -144,17 +145,45 @@ terraclimate_build_slice <- function(longitude,
     start = c(longitude_index, latitude_index, time_index[1]),
     count = c(1, 1, length(time_index)),
     dates = list(
-      start = lubridate::floor_date(required_summary_dates, unit = "1 month"),
-      end = required_summary_dates
+      start = required_summary_dates,
+      end = lubridate::ceiling_date(required_summary_dates, unit = "1 month") -1
     )
   )
 
 }
 
+# spline through dates, fitting a periodic spline to seasonality
+spline_seasonal <- function(values, dates, dates_predict) {
+
+  min_date <- min(dates)
+
+  df_fit <- data.frame(
+    values = values,
+    datenum = as.numeric(dates - min_date),
+    doy = doy(dates)
+  )
+
+  df_predict <- data.frame(
+    datenum = as.numeric(dates_predict - min_date),
+    doy = doy(dates_predict)
+  )
+
+  m <- gam(values ~ s(datenum) + s(doy, bs = "cp"),
+           data = df_fit,
+           # cyclic on days of the year
+           knots = list(doy = c(0.5, 365.5)),
+           # overfit as much as possible
+           gamma = 0.001)
+
+  predict(m, df_predict)
+
+}
+
+
 # Get weather for central Perth WA for 2020-2023
 longitude <- 115.86
 latitude <- -31.95
-dates <- seq(as.Date("2020-01-13"),
+dates <- seq(as.Date("2017-01-13"),
              as.Date("2021-07-20"),
              by = "1 day")
 
@@ -164,34 +193,111 @@ slice <- terraclimate_build_slice(longitude = longitude,
                                   dates = dates)
 
 # get all variables
-vars <- c("tmax", "tmin", "ppt", "ws", "vpd", "srad", "soil")
-results <- list()
+vars <- c("tmax", "tmin", "ppt", "ws", "vpd", "srad")
+climate_monthly <- data.frame(
+  start_date = slice$dates$start,
+  end_date = slice$dates$end,
+  mid_date = slice$dates$start + (slice$dates$end - slice$dates$start) / 2)
 for(var in vars) {
-  results[[var]] <- terraclimate_fetch(slice, var)
+  climate_monthly[, var] <- terraclimate_fetch(slice, var)
 }
 
-climate_data <- cbind(
-  data.frame(
-    start_date = slice$dates$start,
-    end_date = slice$dates$end
-  ),
-  as.data.frame(results)
-)
+# compute relative humidity
 
-climate_data$date <- with(climate_data,
-                          start_date + round((end_date - start_date) / 2))
-plot(tmax ~ date,
-     data = climate_data,
+# mean temperature and vapour pressure
+climate_monthly$tmean <- (climate_monthly$tmax + climate_monthly$tmin) / 2
+climate_monthly$vp <- vapour_pressure(tmean, climate_monthly$vpd)
+# RH at max/min temps
+climate_monthly$rhmax <- relative_humidity(climate_monthly$tmax, climate_monthly$vp)
+climate_monthly$rhmin <- relative_humidity(climate_monthly$tmin, climate_monthly$vp)
+
+
+# plot monthly data
+plot(tmax ~ mid_date,
+     data = climate_monthly,
      ylab = "temperature",
-     ylim = c(0, max(tmax)),
+     xlab = "date",
+     type = "b",
+     ylim = c(min(tmin), max(tmax)),
      pch = 16,
      col = "red")
-points(tmin ~ date,
-       data = climate_data,
+points(tmin ~ mid_date,
+       data = climate_monthly,
        pch = 16,
+       type = "b",
        col = "blue")
 
-# come back and build a version that pulls these out for blocks of pixels at a
-# time, and stores them locally
+# spline these to the requested dates
+climate_daily <- data.frame(
+  date = dates
+)
+new_var <- c("tmax", "tmin", "rhmax", "rhmin", "ws", "srad")
+for (var in new_var) {
+  climate_daily[, var] <- spline_seasonal(values = climate_monthly[, var],
+                                          dates = climate_monthly[, "mid_date"],
+                                          dates_predict = climate_daily[, "date"])
+}
+
+plot(rhmax ~ date,
+     data = climate_daily,
+     type = "l",
+     lwd = 2,
+     pch = 16,
+     col = "blue",
+     ylim = range(c(climate_daily$rhmax, climate_monthly$rhmax)))
+
+points(climate_monthly$rhmax ~ climate_monthly$mid_date,
+       lwd = 1.5,
+       cex = 0.7,
+       col = "blue")
+
+
+
+# need to also obtain:
+#  altitude (DEM lookup, easy)
+#  cloud cover max/min (based on SRAD modification of existing cloudcover)
+
+# from the helpfile for NicheMapR::get.global.climate() :
+
+# Cloud cover comes from a bilinear interpolation of a lower resolution version
+# of this dataset (New, M., M. Hulme, and P. D. Jones. 1999. Representing
+# twentieth century space-time climate variability. Part 1: development of a
+# 1961-90 mean monthly terrestrial climatology. Journal of Climate 12:829-856.).
+
+# need to do some more transformations and wind min/max as per NicheMapR
+# WNMAXX <- WIND
+# WNMINN<-WNMAXX * 0.1 # impose diurnal cycle
+
+# correct for fact that wind is measured at 10 m height
+# wind shear equation v / vo = (h / ho)^a
+#where
+#v = the velocity at height h (m/s)
+#vo = the velocity at height ho (m/s)
+#a = the wind shear exponent
+#Terrain   Wind Shear Exponent
+#- a -
+#  Open water   0.1
+#Smooth, level, grass-covered   0.15 (or more commonly 1/7)
+#Row crops 	0.2
+#Low bushes with a few trees 	0.2
+#Heavy trees 	0.25
+#Several buildings 	0.25
+#Hilly, mountainous terrain 	0.25
+# source http://www.engineeringtoolbox.com/wind-shear-d_1215.html
+
+# WNMINN <- WNMINN * (2 / 10) ^ 0.15
+# WNMAXX <- WNMAXX * (2 / 10) ^ 0.15
+
+
+# then build a wrapper function that:
+
+# 1. downloads terraclimate data in tiles rather than pixels
+
+# 2. downloads and processes to daily or monthly summary data for required
+# quantities
+
+# 3. caches these, and only downloads new ones as needed (at least by year and
+# pixel)
+
 
 
--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,32 @@ check_contiguous_dates <- function(dates) {
   }
 
 }
+
+# compute mean vapour pressure from mean temperature and vapour pressure deficit (kPa)
+vapour_pressure <- function(tmean, vpd_kpa) {
+
+  # Vapour pressure (Pa)
+  vapour_pressure <- NicheMapR::WETAIR(
+    # drybulb temp (C)
+    db = tmean,
+    # max relative humidity
+    rh = 100)$e
+
+  # subtract vapour pressure deficit from hypothetical amount to get actual
+
+  # convert units
+  vpd_pa <- vpd_kpa * 1000
+  vapour_pressure - vpd_pa
+
+}
+
+relative_humidity <- function(temperature, vapour_pressure) {
+  # compute saturation vapour pressure (Pa)
+  saturation_vapour_pressure <- NicheMapR::WETAIR(db = temperature,
+                                                  rh = 100)$esat
+  # compute and clamp relative humidity
+  rh <- (vapour_pressure / saturation_vapour_pressure) * 100
+  rh <- pmin(rh, 100)
+  rh <- pmax(rh, 0.01)
+  rh
+}
