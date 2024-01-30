c41eb50d45180153f292d5a51bce94d959add290
Nick Golding <goldingn35@gmail.com>
2024-01-30 17:37:04 +0800

    tune splining

--- R/query_terraclimate.R
+++ R/query_terraclimate.R
@@ -1,6 +1,9 @@
 # read in terraclimate data
 library(ncdf4)
 library(lubridate)
+library(mgcv)
+
+source("R/utils.R")
 
 # get the URL to the terraclimate monthly summary data
 terraclimate_url <- function(variable = c("tmax", "tmin", "ppt", "ws", "vpd", "srad")) {
@@ -71,11 +74,11 @@ days_since_1900 <- function() {
 
 # return a sequence of last days of the month, for all months encompassing the
 # requested dates
-monthly_summary_days <- function(dates) {
-  # find the start of month immediately before the first date
-  min_date <- lubridate::floor_date(min(dates), unit = "month")
-  # find the start of month immediately after the last date
-  max_date <- lubridate::ceiling_date(max(dates), unit = "month")
+monthly_summary_days <- function(dates, buffer_days = 183) {
+  # start of month buffer_days before the first date
+  min_date <- lubridate::floor_date(min(dates) - buffer_days, unit = "month")
+  # start of month buffer_days after the last date
+  max_date <- lubridate::floor_date(max(dates) + buffer_days, unit = "month")
   # sequence of all the first dates of the months in between
   seq(min_date, max_date, by = "1 month")
 }
@@ -120,7 +123,8 @@ terraclimate_build_slice <- function(longitude,
   check_dates(dates, available)
 
   # the timeseries is summarised by month and indexed by the first day of the
-  # month, so find all summary dates relevant to our target dates
+  # month, so find all summary dates relevant to our target dates, with some
+  # buffering either side to make sure the splining is consistent for those dates
   required_summary_dates <- monthly_summary_days(dates)
 
   # the terraclimate temporal index is integer days since the first day of 1900
@@ -128,12 +132,14 @@ terraclimate_build_slice <- function(longitude,
   # month-start dates as indices
   time_index_1900 <- match(required_summary_dates, days_since_1900()) - 1
 
-  # I'm insecure
-  if (!all(time_index_1900 %in% available$times)) {
-    stop("Some date error. It's Nick's fault, please tell him.")
-  }
+  # Remove any of these months that are not available. The dates themselves are
+  # all included (checked above), so this is just removing buffer dates we can't
+  # access.
+  indices_keep <- time_index_1900 %in% available$times
+  required_summary_dates <- required_summary_dates[indices_keep]
+  time_index_1900 <- time_index_1900[indices_keep]
 
-  # now index the elements of the NCDF array - accounting for indexing from 0 in NCDF
+  # now index the elements of the NCDF array
   time_index <- match(time_index_1900, available$times)
 
   # Find the nearest terraclimate lat longs to the ones we want
@@ -153,7 +159,9 @@ terraclimate_build_slice <- function(longitude,
 }
 
 # spline through dates, fitting a periodic spline to seasonality
-spline_seasonal <- function(values, dates, dates_predict) {
+spline_seasonal <- function(values, dates, dates_predict,
+                            knots_per_year_trend = 2,
+                            knots_per_year_season = 6) {
 
   min_date <- min(dates)
 
@@ -168,12 +176,27 @@ spline_seasonal <- function(values, dates, dates_predict) {
     doy = doy(dates_predict)
   )
 
-  m <- gam(values ~ s(datenum) + s(doy, bs = "cp"),
-           data = df_fit,
-           # cyclic on days of the year
-           knots = list(doy = c(0.5, 365.5)),
-           # overfit as much as possible
-           gamma = 0.001)
+  # adjust knots based on length of timeseries
+  duration_years <- max(df_fit$datenum) / 365
+
+  # trend, based on number of years
+  knots_trend <- ceiling(knots_per_year_trend * duration_years)
+
+  # seasonal, based on length if less than one year
+  fraction_of_year <- pmin(duration_years, 1)
+  knots_season <- ceiling(knots_per_year_season * fraction_of_year)
+
+  # but we need at least 2 knots on each
+  knots_trend <- pmax(knots_trend, 2)
+  knots_season <- pmax(knots_season, 2)
+
+  m <- mgcv::gam(values ~ s(datenum, k = knots_trend) +
+                   s(doy, bs = "cp", k = knots_season),
+                 data = df_fit,
+                 # cyclic on days of the year
+                 knots = list(doy = c(0.5, 365.5)),
+                 # overfit as much as possible
+                 gamma = 0.1)
 
   predict(m, df_predict)
 
@@ -183,8 +206,8 @@ spline_seasonal <- function(values, dates, dates_predict) {
 # Get weather for central Perth WA for 2020-2023
 longitude <- 115.86
 latitude <- -31.95
-dates <- seq(as.Date("2017-01-13"),
-             as.Date("2021-07-20"),
+dates <- seq(as.Date("2010-03-01"),
+             as.Date("2020-12-31"),
              by = "1 day")
 
 # build the indexing slice
@@ -206,26 +229,10 @@ for(var in vars) {
 
 # mean temperature and vapour pressure
 climate_monthly$tmean <- (climate_monthly$tmax + climate_monthly$tmin) / 2
-climate_monthly$vp <- vapour_pressure(tmean, climate_monthly$vpd)
+climate_monthly$vp <- vapour_pressure(climate_monthly$tmean, climate_monthly$vpd)
 # RH at max/min temps
 climate_monthly$rhmax <- relative_humidity(climate_monthly$tmax, climate_monthly$vp)
 climate_monthly$rhmin <- relative_humidity(climate_monthly$tmin, climate_monthly$vp)
-
-
-# plot monthly data
-plot(tmax ~ mid_date,
-     data = climate_monthly,
-     ylab = "temperature",
-     xlab = "date",
-     type = "b",
-     ylim = c(min(tmin), max(tmax)),
-     pch = 16,
-     col = "red")
-points(tmin ~ mid_date,
-       data = climate_monthly,
-       pch = 16,
-       type = "b",
-       col = "blue")
 
 # spline these to the requested dates
 climate_daily <- data.frame(
@@ -238,20 +245,31 @@ for (var in new_var) {
                                           dates_predict = climate_daily[, "date"])
 }
 
-plot(rhmax ~ date,
-     data = climate_daily,
-     type = "l",
-     lwd = 2,
-     pch = 16,
-     col = "blue",
-     ylim = range(c(climate_daily$rhmax, climate_monthly$rhmax)))
+# it would be nice if we could use the correct likelihood for disaggregation
+# here, but this is cheap and probably not much different
 
-points(climate_monthly$rhmax ~ climate_monthly$mid_date,
-       lwd = 1.5,
-       cex = 0.7,
-       col = "blue")
+par(mfrow = c(1, 1))
+op <- par(mfrow = n2mfrow(length(new_var)),
+          mar = c(3, 3, 2, 1))
+for (var in new_var) {
+  ylims <- range(c(climate_daily[, var], climate_monthly[, var]))
+  xlims <- range(c(climate_daily$date, climate_monthly$mid_date))
+  plot(climate_daily[, var] ~ climate_daily$date,
+       type = "l",
+       col = grey(0.4),
+       lwd = 2,
+       ylab = "",
+       xlab = "",
+       main = var,
+       pch = 16,
+       ylim = ylims,
+       xlim = xlims)
 
-
+  points(climate_monthly[, var] ~ climate_monthly$mid_date,
+         lwd = 1.5,
+         cex = 0.7)
+}
+par(op)
 
 # need to also obtain:
 #  altitude (DEM lookup, easy)
@@ -265,39 +283,44 @@ points(climate_monthly$rhmax ~ climate_monthly$mid_date,
 # 1961-90 mean monthly terrestrial climatology. Journal of Climate 12:829-856.).
 
 # need to do some more transformations and wind min/max as per NicheMapR
+
 # WNMAXX <- WIND
 # WNMINN<-WNMAXX * 0.1 # impose diurnal cycle
 
-# correct for fact that wind is measured at 10 m height
-# wind shear equation v / vo = (h / ho)^a
-#where
-#v = the velocity at height h (m/s)
-#vo = the velocity at height ho (m/s)
-#a = the wind shear exponent
-#Terrain   Wind Shear Exponent
-#- a -
-#  Open water   0.1
-#Smooth, level, grass-covered   0.15 (or more commonly 1/7)
-#Row crops 	0.2
-#Low bushes with a few trees 	0.2
-#Heavy trees 	0.25
-#Several buildings 	0.25
-#Hilly, mountainous terrain 	0.25
+# Correct for fact that wind is measured at 10m height
+# wind shear equation: v / vo = (h / ho)^a
+# where
+# v = the velocity at height h (m/s)
+# vo = the velocity at height ho (m/s)
+# a = the wind shear exponent
+
+# Terrain                       Wind Shear Exponent
+# -----------------------------|---------------------------
+# Open water                    0.1
+# Smooth, level, grass-covered  0.15 (or more commonly 1/7)
+# Row crops 	                  0.2
+# Low bushes with a few trees 	0.2
+# Heavy trees 	                0.25
+# Several buildings 	          0.25
+# Hilly, mountainous terrain 	  0.25
+
 # source http://www.engineeringtoolbox.com/wind-shear-d_1215.html
 
 # WNMINN <- WNMINN * (2 / 10) ^ 0.15
 # WNMAXX <- WNMAXX * (2 / 10) ^ 0.15
 
+# add this wind shear exponent to the microclimate simulation set up?
+
 
 # then build a wrapper function that:
 
-# 1. downloads terraclimate data in tiles rather than pixels
+# 1. Downloads terraclimate data in tiles rather than pixels
 
-# 2. downloads and processes to daily or monthly summary data for required
-# quantities
+# 2. Downloads, processes, and saves as monthly daily max and min (then spline
+# later) summary data for required quantities
 
-# 3. caches these, and only downloads new ones as needed (at least by year and
-# pixel)
+# 3. Caches these, and only downloads new pixels/times as needed (at least by
+# month and pixel)
 
 
 
