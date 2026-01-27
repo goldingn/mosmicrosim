6096e9ed3d3bec8e580dee26c208eec0b3997067
Nick Golding <goldingn35@gmail.com>
2026-01-27 14:44:26 +0300

    output anomaly plots

--- make_figs.R
+++ make_figs.R
@@ -233,3 +233,128 @@ ggsave(
   height = 6.5
 )
 
+
+# animate a gif of all the slice data
+
+ag_animate <- ag
+animate_names <- paste(slice_year, month.name[slice_month])
+names(ag_animate) <- animate_names
+
+library(gganimate)
+slice_max <- max(terra::global(ag_animate, "max", na.rm = TRUE))
+
+slice_plot <- ggplot() +
+  geom_spatraster(
+    data = ag_animate[[1:3]]
+  ) +
+  facet_wrap(
+    ~lyr
+  ) +
+  country_borders +
+  scale_fill_distiller(
+    guide = "none",
+    palette = "Purples",
+    direction = 1,
+    limits = c(0, slice_max),
+    transform = "log1p",
+    na.value = "transparent"
+  ) +
+  theme_maps()
+
+slice_plot_animated <- slice_plot +
+  transition_states(
+    lyr,
+    transition_length = 1,
+    state_length = 0
+  )
+
+anim <- gganimate::animate(slice_plot_animated, fps = 2)
+gganimate::anim_save("~/Desktop/mosmicrosim_animation.gif",
+                     anim)
+
+magick::image_write(anim,
+                    path = "~/Desktop/mosmicrosim_animation.gif")
+
+
+
+
+# create monthly anomaly maps
+
+idx_2020 <- which(slice_year == 2020)
+anomaly_2020 <- synoptic - ag[[idx_2020]]
+
+
+names(anomaly_2020) <- month.name[slice_month[idx_2020]]
+
+anomaly_max <- max(terra::global(anomaly_2020, "max", na.rm = TRUE))
+anomaly_min <- min(terra::global(anomaly_2020, "min", na.rm = TRUE))
+anomaly_extreme <- max(abs(anomaly_min), abs(anomaly_max))
+anomaly_range <- c(-anomaly_extreme, anomaly_extreme)
+
+anomaly_2020_plot <- ggplot() +
+  geom_spatraster(
+    data = anomaly_2020
+  ) +
+  facet_wrap(
+    ~lyr
+  ) +
+  country_borders +
+  scale_fill_distiller(
+    guide = "none",
+    palette = "RdBu",
+    direction = 1,
+    limits = anomaly_range,
+    na.value = "transparent"
+  ) +
+  theme_maps() +
+  ggtitle(
+    "Climatic abundance anomaly in 2020",
+    "Difference from 2000-2024 mean vector abundance per unit of larval habitat"
+  )
+
+anomaly_2020_plot_fpath <- file.path(
+  "figures",
+  "anomaly_2020.png"
+)
+
+ggsave(
+  filename = anomaly_2020_plot_fpath,
+  plot = anomaly_2020_plot,
+  bg = "white",
+  width = 6.5,
+  height = 6.5
+)
+
+
+# zoom in on anomaly in May 2020
+
+anomaly_may_2020_plot <- ggplot() +
+  geom_spatraster(
+    data = anomaly_2020[["May"]]
+  ) +
+  country_borders +
+  scale_fill_distiller(
+    guide = "none",
+    palette = "RdBu",
+    direction = 1,
+    limits = anomaly_range,
+    na.value = "transparent"
+  ) +
+  coord_sf(
+    xlim = c(-1, 50),
+    ylim = c(-17, 15)
+  ) +
+  theme_maps()
+
+anomaly_may_2020_plot_fpath <- file.path(
+  "figures",
+  "anomaly_may_2020.png"
+)
+
+ggsave(
+  filename = anomaly_may_2020_plot_fpath,
+  plot = anomaly_may_2020_plot,
+  bg = "white",
+  width = 6.5,
+  height = 6.5
+)
